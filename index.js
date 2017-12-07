0ebd44211dd3fbc8ccbdda92bb2a703caaa1875c
Boyuzhou <2470200492@qq.com>
2017-12-07 19:03:39 +0800

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+
+exports.bugfree = require('./lib/bugfree');
+
+exports.randomString = require('./lib/randomString');
+
+exports.moment = require('./lib/moment');
+
+exports.warning = require('./lib/warning');
+
+exports.encrypt = require('./lib/encrypt');
+
